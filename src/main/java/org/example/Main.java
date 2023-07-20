13aaae1b60205ae338578ce0eb1ce38f59e7c5d8
wesley <wesleyemidio99@hotmail.com>
2023-07-20 20:58:21 -0300

    Digitando as informações que deseja salvar

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner digitar = new Scanner(System.in);
+        System.out.println("Banco");
 
         System.out.println("Banco");
         System.out.println("Digite as informações");
