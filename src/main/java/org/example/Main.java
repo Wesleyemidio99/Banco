4a29c37be5c8ed0c0ea06da7312d6f3760bef275
wesley <wesleyemidio99@hotmail.com>
2023-07-20 20:54:26 -0300

    Inicializacao de projeto com informações pre salva.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,22 +1,39 @@
 package org.example;
 
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Banco");
+        Scanner digitar = new Scanner(System.in);
 
-        Conta conta = new Conta("Wesley", "457.545.849-54","28/07/1999");
+        System.out.println("Banco");
+        System.out.println("Digite as informações");
+        System.out.println("Digite seu nome");
+        String nome = digitar.nextLine();
+        System.out.println("Digite seu CPF");
+        String cpf = digitar.nextLine();
+        System.out.println("Digite sua data de nascimento");
+        String dataNascimento = digitar.nextLine();
+
+        Conta conta = new Conta(nome, cpf,dataNascimento); //Criando o Objeto Conta
 
         System.out.println(conta.getNome());
         System.out.println(conta.getCpf());
         System.out.println(conta.getDataNascimento());
 
-        conta.depositar(1000);
+        System.out.println("Ditie a quantidade que deseja depositar");
+        int quantidadeDepositar = digitar.nextInt();
+        conta.depositar(quantidadeDepositar);
 
-        System.out.println(conta.getSaldo());
+        System.out.println("Seu saldo = "+ conta.getSaldo());
 
-        conta.sacar(50);
-        System.out.println(conta.getSaldo());
+        System.out.println("Digite a quantidade para fazer o saque:");
+        int quantidadeSaque = digitar.nextInt();
+
+        conta.sacar(quantidadeSaque);
+        System.out.println("Quantidade restante do saldo = " + conta.getSaldo());
+
+        digitar.close();
 
     }
 
