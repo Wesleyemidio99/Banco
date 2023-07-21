b3eebbe2a3400bdc4e0f228a247002b10dc4cc9a
wesley <wesleyemidio99@hotmail.com>
2023-07-20 21:12:30 -0300

    Criar menu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,9 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner digitar = new Scanner(System.in);
-        System.out.println("Banco");
 
-        System.out.println("Banco");
         System.out.println("Digite as informações");
         System.out.println("Digite seu nome");
         String nome = digitar.nextLine();
@@ -18,21 +16,36 @@ public class Main {
 
         Conta conta = new Conta(nome, cpf,dataNascimento); //Criando o Objeto Conta
 
-        System.out.println(conta.getNome());
-        System.out.println(conta.getCpf());
-        System.out.println(conta.getDataNascimento());
+        System.out.println("Digite qual opcao deseja:");
+        System.out.println("1- Verificar cadastro / 2 - saldo / 3 - deposito / 4 - saque");
+        int opcao = digitar.nextInt();
 
-        System.out.println("Ditie a quantidade que deseja depositar");
-        int quantidadeDepositar = digitar.nextInt();
-        conta.depositar(quantidadeDepositar);
+        switch (opcao){
+            case 1:
 
-        System.out.println("Seu saldo = "+ conta.getSaldo());
+                System.out.println(conta.getNome());
+                System.out.println(conta.getCpf());
+                System.out.println(conta.getDataNascimento());
 
-        System.out.println("Digite a quantidade para fazer o saque:");
-        int quantidadeSaque = digitar.nextInt();
+                break;
 
-        conta.sacar(quantidadeSaque);
-        System.out.println("Quantidade restante do saldo = " + conta.getSaldo());
+            case 2:
+                System.out.println("O seu saldo = " + conta.getSaldo());
+                break;
+
+            case 3:
+                System.out.println("Digite a quantidade que deseja depositar");
+                int quantidadeDepositar = digitar.nextInt();
+                conta.depositar(quantidadeDepositar);
+                System.out.println("Quantidade do saldo = " + conta.getSaldo());
+                break;
+
+            case 4:
+                System.out.println("Digite a quantidade para fazer o saque:");
+                int quantidadeSaque = digitar.nextInt();
+                conta.sacar(quantidadeSaque);
+                System.out.println("Quantidade restante do saldo = " + conta.getSaldo());
+        }
 
         digitar.close();
 
